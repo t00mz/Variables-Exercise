b3b19023cbb02718f29d86601c5c9d1f61e6eead
Michael Hatoum <hatoumm89@gmail.com>
2022-10-14 15:39:40 -0400

    done

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
+            string dogName = "Rex";
+            int dogAge = 6;
+            bool myDogIsWhite = true;
+            double dogHeight = 3.5;
+            char nrOfLegs = '4';
+            decimal dogWidth = 21.2341m;
+
+            
+            Console.WriteLine($"My Dog's name is {dogName}, he's {dogAge} years old, and he has {nrOfLegs} legs.");
+            Console.WriteLine($"My dog is white;it's {myDogIsWhite}");
     }
 }
